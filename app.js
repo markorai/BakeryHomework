c0887128153e21a776209247e724d489ada329cf
Marko Raicevic <markoraicevic649@gmail.com>
2021-10-20 23:43:46 +0200

    Small update to app.js file about image visibility

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const toggleButton = document.getElementsByClassName("toggle_button")[0];
 const navLinks = document.getElementsByClassName("nav-links")[0];
+const image = document.getElementById("changing-img");
 toggleButton.addEventListener("click",()=>{
     navLinks.classList.toggle("active");
+    image.classList.toggle("hide-image");
 })
