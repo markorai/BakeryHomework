82fb32e64172398bb7e260426f89b53d0fca8d03
Marko Raicevic <markoraicevic649@gmail.com>
2021-10-22 01:40:18 +0200

    Added JS to hide caption on mobile when navbar is open

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const toggleButton = document.getElementsByClassName("toggle_button")[0];
 const navLinks = document.getElementsByClassName("nav-links")[0];
-const image = document.getElementById("changing-img");
+const mainCaption = document.getElementsByClassName("main-caption")[0];
+const mainDescription = document.getElementsByClassName("main-description")[0];
 toggleButton.addEventListener("click",()=>{
     navLinks.classList.toggle("active");
-    image.classList.toggle("hide-image");
+    mainCaption.classList.toggle("hide");
+    mainDescription.classList.toggle("hide");
 })
